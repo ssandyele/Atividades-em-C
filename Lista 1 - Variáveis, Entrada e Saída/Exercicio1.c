4d0d48fb973ba966b1b6d47731feb6788d5ac492
Sandyele Severo <severosandyele@gmail.com>
2025-07-11 17:38:37 -0300

    adiciona estrutura inicial dos 15 exercícios da Lista 1 (sem resolução)

---
+++ Lista 1 - Variáveis, Entrada e Saída/Exercicio1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Elabore um programa que escreva as mensagens “Início do programa” e
+“Fim” na tela, uma em cada linha, usando apenas um comando printf().*/
+
+int main () {
+    printf("Inicio do programa\nFim\n");
+    return 0;
+}
